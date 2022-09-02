2ae76bf0a0e6838fd0a55a4ee0126ed5aa004988
ViniciusCSantos <68887126+ViniciusCavalcanteSantos@users.noreply.github.com>
2022-09-02 19:32:55 -0300

    Adiciona animação de entrada ao mostruário

--- js/script.js
+++ js/script.js
@@ -7,3 +7,12 @@ function stickyNavbar() {
 }
 
 window.addEventListener("scroll", stickyNavbar);
+
+/* ------------- ANIMAÇÃO DE REVEAL ------------- */
+let sr = ScrollReveal({
+  duration: 2500,
+  distance: "60px"
+})
+
+sr.reveal(".showcase-info", {delay: 600});
+sr.reveal(".showcase-image", {origin: "top", delay: 700});
