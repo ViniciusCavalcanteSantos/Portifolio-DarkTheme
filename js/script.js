b770cba6daf3729d6e16f074198005fd91e65c29
ViniciusCSantos <68887126+ViniciusCavalcanteSantos@users.noreply.github.com>
2022-09-07 17:47:38 -0300

    Adiciona a seção "habilidades"

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 const header = document.querySelector(".header");
 
+const first_skill = document.querySelector(".skill:first-child");
+const sk_counters = document.querySelectorAll(".counter span");
+const progress_bars = document.querySelectorAll(".sk-progress svg circle");
+
+window.addEventListener("scroll", () => {
+  if(!skillsPlayed) skillsCounter();
+})
+
 /* ------------- NAVBAR ------------- */
 
 function stickyNavbar() {
@@ -16,3 +24,44 @@ let sr = ScrollReveal({
 
 sr.reveal(".showcase-info", {delay: 300});
 sr.reveal(".showcase-image", {origin: "top", delay: 400});
+
+/* ------------- BARRA DE PROGRESSO DAS SKILLS ------------- */
+function updateCount(counter, maxNum) {
+  let currentNum = +counter.innerText;
+
+  if(currentNum < maxNum) {
+    counter.innerText = currentNum + 1;
+
+    setTimeout(() => {
+      updateCount(counter, maxNum);      
+    }, 12)
+  }
+}
+
+function hasReached(el) {
+  let topPosition = el.getBoundingClientRect().top;
+
+  if(window.innerHeight >= topPosition + el.offsetHeight) {
+    return true;
+  }
+
+  return false;
+}
+
+let skillsPlayed = false;
+function skillsCounter() {
+  if(!hasReached(first_skill)) return;
+  
+  skillsPlayed = true;
+
+  sk_counters.forEach((counter, i) => {
+    let target = +counter.dataset.target;
+    let strokeValue = 427 - 427 * (target / 100)
+
+    progress_bars[i].style.setProperty("--target", strokeValue);
+
+    setTimeout(() => {updateCount(counter, target)}, 400);
+  })
+
+  progress_bars.forEach(p => p.style.animation = "progress 2s ease-in-out forwards")
+}
